ce4159d9a205d0708d8118c847e9531d8e8cb0fd
Gitesh-Jawale <131944512+Gitesh-Jawale@users.noreply.github.com>
2023-07-03 18:39:48 +0530

    Update cloning.groovy

--- vars/cloning.groovy
+++ vars/cloning.groovy
@@ -1,5 +1,7 @@
 def call (String url, String branch) {
     echo "hi sagar"
-    git branch: '${branch}' , url: '${url}'
+    git branch: "${branch}" , url: "${url}"
+    echo "${url}"
+    echo "Gitesh"
     
 }
